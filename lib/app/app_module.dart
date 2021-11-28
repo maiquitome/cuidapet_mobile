5b144158c0f667421d040e9ff3368884b2aa03ff
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-11-28 13:27:28 -0300

    :wrench: initial config

---
+++ lib/app/app_module.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  final List<Bind<Object>> binds = <Bind<Object>>[];
+
+  @override
+  final List<ModularRoute> routes = <ModularRoute>[];
+}
---
+++ lib/app/app_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'package:asuka/asuka.dart' as asuka;
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      builder: () => MaterialApp(
+        builder: asuka.builder,
+        navigatorObservers: <NavigatorObserver>[
+          asuka.asukaHeroController
+        ],
+      ).modular(),
+    );
+  }
+}
